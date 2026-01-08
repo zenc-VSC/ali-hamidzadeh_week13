cb3e38d752924b12cfdda330eab33a0d2d5370c9
Ali Hamidzadeh <hamidzadeh.80.ali@gmail.com>
2026-01-08 18:59:05 +0330

    Create Tab component with 5-tab layout and placeholder content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Tab  from "./Tab.jsx";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>BotoStart | Tab Component project</h1>
+      <Tab />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Tab.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+function Tab() {
+  return (
+    <div className="container">
+      <div className="tabs-container">
+        <div className="tab">Tab 1</div>
+        <div className="tab">Tab 2</div>
+        <div className="tab">Tab 3</div>
+        <div className="tab">Tab 4</div>
+        <div className="tab">Tab 5</div>
+      </div>
+      <div className="tabs-content">
+        <div className="content">
+          <h2>Content 1</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
+        <div className="content">
+          <h2>Content 2</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
+        <div className="content">
+          <h2>Content 3</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
+        <div className="content">
+          <h2>Content 4</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
+        <div className="content">
+          <h2>Content 5</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tab;
