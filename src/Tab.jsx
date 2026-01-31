c60a226e9da087179c074340d1beea0df8a46ea1
Ali Hamidzadeh <hamidzadeh.80.ali@gmail.com>
2026-01-31 23:14:54 +0330

    Fix Tab component logic and styling

--- src/Tab.jsx
+++ src/Tab.jsx
@@ -1,93 +1,42 @@
 import React, { useState } from "react";
 import "./Tab.css";
 
+const tabData = [
+  { id: 0, title: "Tab 1", content: "Content 1" },
+  { id: 1, title: "Tab 2", content: "Content 2" },
+  { id: 2, title: "Tab 3", content: "Content 3" },
+  { id: 3, title: "Tab 4", content: "Content 4" },
+  { id: 4, title: "Tab 5", content: "Content 5" },
+];
+
 function Tab() {
   const [activeTab, setActiveTab] = useState(0);
 
   return (
     <div className="container">
       <div className="tabs-container">
-        <div className="tab active" onClick={() => setActiveTab(0)}>
-          Tab 1
-        </div>
-        <div className="tab" onClick={() => setActiveTab(1)}>
-          Tab 2
-        </div>
-        <div className="tab" onClick={() => setActiveTab(2)}>
-          Tab 3
-        </div>
-        <div className="tab" onClick={() => setActiveTab(3)}>
-          Tab 4
-        </div>
-        <div className="tab" onClick={() => setActiveTab(4)}>
-          Tab 5
-        </div>
+        {tabData.map((tab, index) => (
+          <div
+            key={tab.id}
+            className={`tab ${activeTab === index ? "active" : ""}`}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab.title}
+          </div>
+        ))}
       </div>
+
       <div className="tabs-content">
-        {activeTab === 0 && (
-          <div className="content">
-            <h2>Content 1</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              ducimus quibusdam dolore expedita cumque iste iusto laboriosam,
-              quaerat quo eaque voluptates consequatur? Accusantium cumque culpa
-              maxime quibusdam qui ipsa nihil!
-            </p>
-          </div>
-        )}
-
-        {activeTab === 1 && (
-          <div className="content">
-            <h2>Content 2</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              ducimus quibusdam dolore expedita cumque iste iusto laboriosam,
-              quaerat quo eaque voluptates consequatur? Accusantium cumque culpa
-              maxime quibusdam qui ipsa nihil!
-            </p>
-          </div>
-        )}
-
-        {activeTab === 2 && (
-          <div className="content">
-            <h2>Content 3</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              ducimus quibusdam dolore expedita cumque iste iusto laboriosam,
-              quaerat quo eaque voluptates consequatur? Accusantium cumque culpa
-              maxime quibusdam qui ipsa nihil!
-            </p>
-          </div>
-        )}
-
-        {activeTab === 3 && (
-          <div className="content">
-            <h2>Content 4</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              ducimus quibusdam dolore expedita cumque iste iusto laboriosam,
-              quaerat quo eaque voluptates consequatur? Accusantium cumque culpa
-              maxime quibusdam qui ipsa nihil!
-            </p>
-          </div>
-        )}
-
-        {activeTab === 4 && (
-          <div className="content">
-            <h2>Content 5</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-              ducimus quibusdam dolore expedita cumque iste iusto laboriosam,
-              quaerat quo eaque voluptates consequatur? Accusantium cumque culpa
-              maxime quibusdam qui ipsa nihil!
-            </p>
-          </div>
-        )}
+        <div className="content">
+          <h2>{tabData[activeTab].content}</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ducimus
+            quibusdam dolore expedita cumque iste iusto laboriosam, quaerat quo
+            eaque voluptates consequatur? Accusantium cumque culpa maxime
+            quibusdam qui ipsa nihil!
+          </p>
+        </div>
       </div>
     </div>
   );
